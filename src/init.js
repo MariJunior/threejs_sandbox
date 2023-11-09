7ccab8c85989232e7f2741cadbdc902e0720e3a2
Marina Kalyuzhnaya <marinakaluzhnaya13@gmail.com>
2023-11-09 19:27:43 +0300

    feat: 🎸 add grid with geometries and controls + GUI for debugging

--- src/init.js
+++ src/init.js
@@ -1,25 +1,36 @@
+import * as dat from 'lil-gui';
+import Stats from 'stats.js';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const init = () => {
-	const sizes = {
-		width: window.innerWidth,
-		height: window.innerHeight,
-	};
+  const canvas = document.getElementById('threejs-canvas');
 
-	const scene = new THREE.Scene();
-	const canvas = document.querySelector('.canvas');
-	const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-	scene.add(camera);
+  const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
 
-	const controls = new OrbitControls(camera, canvas);
-	controls.enableDamping = true;
+  const scene = new THREE.Scene();
 
-	const renderer = new THREE.WebGLRenderer({ canvas });
-	renderer.setSize(sizes.width, sizes.height);
-	renderer.render(scene, camera);
+  const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+  scene.add(camera);
 
-	return { sizes, scene, canvas, camera, renderer, controls };
+  const controls = new OrbitControls(camera, canvas);
+  controls.enableDamping = true;
+
+  // Вызываем отображение статистики приложения. По умолчанию — панель FPS
+  const stats = new Stats();
+  stats.showPanel(0);
+  document.body.appendChild(stats.dom);
+
+  const gui = new dat.GUI({ closeFolders: true });
+
+  const renderer = new THREE.WebGLRenderer({ canvas });
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.render(scene, camera);
+
+  return { sizes, scene, canvas, camera, renderer, controls, stats, gui };
 };
 
 export default init;
--- src/script.js
+++ src/script.js
@@ -1,48 +1,191 @@
+import debounce from 'lodash/debounce';
 import * as THREE from 'three';
-
+import TWEEN from 'three/examples/jsm/libs/tween.module';
 import init from './init';
 
 import './style.css';
 
-const { sizes, camera, scene, canvas, controls, renderer } = init();
+const { sizes, camera, scene, canvas, controls, renderer, stats, gui } = init();
 
-camera.position.z = 3;
+camera.position.z = 22;
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
-	color: 'gray',
-	wireframe: true,
-});
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const figuresGroup = new THREE.Group();
+
+const GEOMETRIES = [
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.ConeGeometry(1, 2, 16, 4),
+  new THREE.CylinderGeometry(0.75, 1, 2, 16, 4),
+  new THREE.TetrahedronGeometry(1, 0),
+  new THREE.TorusGeometry(1, 0.5, 16, 100),
+  new THREE.TorusKnotGeometry(1, 0.25, 100, 16, 1, 5),
+  new THREE.DodecahedronGeometry(1, 0),
+  new THREE.OctahedronGeometry(1, 0),
+  new THREE.SphereGeometry(1, 32, 16),
+];
+
+const COLORS = [0x53e0a7, 0xec933b, 0xd5a5ff, 0xa5ffa8, 0xfaffa5];
+
+let geometryIndex = 0;
+let activeIndex = -1;
+
+// Создаём и заполняем сетку из массива заданных геометрий
+for (let i = -5; i <= 5; i += 5) {
+  for (let j = -5; j <= 5; j += 5) {
+    const parameters = {
+      color: 'gray',
+    };
+
+    const material = new THREE.MeshBasicMaterial({
+      color: parameters.color,
+      wireframe: true,
+    });
+
+    const mesh = new THREE.Mesh(GEOMETRIES[geometryIndex], material);
+    const geometryType = GEOMETRIES[geometryIndex].type;
+
+    const figureGUIFolder = gui.addFolder(
+      `${geometryType} [${i}, ${j}] settings`,
+    );
+
+    figureGUIFolder.add(mesh, 'visible').name('Is visible?');
+    figureGUIFolder
+      .add(mesh.scale, 'x')
+      .min(1)
+      .max(4)
+      .step(0.25)
+      .name('Scale x');
+    figureGUIFolder
+      .add(mesh.scale, 'y')
+      .min(1)
+      .max(4)
+      .step(0.25)
+      .name('Scale y');
+    figureGUIFolder.add(mesh.material, 'wireframe').name('Is wireframed?');
+    figureGUIFolder
+      .addColor(parameters, 'color')
+      .onChange(() => {
+        material.color.set(parameters.color);
+      })
+      .name('Color');
+    mesh.position.set(i, j, 10);
+    mesh.currentIndex = geometryIndex;
+    mesh.basicPosition = new THREE.Vector3(i, j, 10);
+    figuresGroup.add(mesh);
+
+    geometryIndex += 1;
+  }
+}
+
+scene.add(figuresGroup);
+
+const resetActiveFigure = () => {
+  figuresGroup.children[activeIndex].material.color.set('gray');
+  // При помощи GSAP анимируем приближение выбранной фигуры
+  new TWEEN.Tween(figuresGroup.children[activeIndex].position)
+    .to(
+    {
+      x: figuresGroup.children[activeIndex].basicPosition.x,
+      y: figuresGroup.children[activeIndex].basicPosition.y,
+      z: figuresGroup.children[activeIndex].basicPosition.z,
+    },
+      Math.random() * 1000 + 500,
+    )
+    .easing(TWEEN.Easing.Exponential.InOut)
+    .start();
+  activeIndex = -1;
+};
+
+const clock = new THREE.Clock();
 
 const tick = () => {
-	controls.update();
-	renderer.render(scene, camera);
-	window.requestAnimationFrame(tick);
+  stats.begin();
+  const delta = clock.getDelta();
+
+  if (activeIndex !== -1) {
+    // Вращаем нашу активную фигурку
+    figuresGroup.children[activeIndex].rotation.y += delta * 0.5;
+  }
+
+  TWEEN.update();
+  controls.update();
+  renderer.render(scene, camera);
+  stats.end();
+  window.requestAnimationFrame(tick);
 };
 tick();
 
-/** Базовые обпаботчики событий длы поддержки ресайза */
-window.addEventListener('resize', () => {
-	// Обновляем размеры
-	sizes.width = window.innerWidth;
-	sizes.height = window.innerHeight;
+// Добавим отслеживание кликов по фигурам
+// В данной ситуации обычный addEventListener на click не подойдёт, т.к.
+// сетки — сущности внутри Canvas, а не DOM
+// Таким образом, нам нужен функционал Raycaster https://threejs.org/docs/#api/en/core/Raycaster
+// TODO: доработать ограничение клика при нажатии на объект в центре:
+// если есть активный,убирать его, если нет активного — выбирать центральный
+const raycaster = new THREE.Raycaster();
+const handleFigureClick = (event) => {
+  const pointer = new THREE.Vector2();
+  // Нормализуем значения координат курсора
+  pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+  pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
-	// Обновляем соотношение сторон камеры
-	camera.aspect = sizes.width / sizes.height;
-	camera.updateProjectionMatrix();
+  raycaster.setFromCamera(pointer, camera);
 
-	// Обновляем renderer
-	renderer.setSize(sizes.width, sizes.height);
-	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-	renderer.render(scene, camera);
-});
+  const figuresClickIntersections = raycaster.intersectObjects(
+    figuresGroup.children,
+  );
 
-window.addEventListener('dblclick', () => {
-	if (!document.fullscreenElement) {
-		canvas.requestFullscreen();
-	} else {
-		document.exitFullscreen();
-	}
-});
+  // Сбрасываем состояние до дефолтного у предыдущей активной фигуры
+  if (activeIndex !== -1) resetActiveFigure();
+
+  figuresClickIntersections.forEach((figure) => {
+    figure.object.material.color.set(COLORS[Math.floor(Math.random() * 4 + 1)]);
+
+    activeIndex = figure.object.currentIndex;
+
+    // При помощи GSAP анимируем приближение выбранной фигуры
+    new TWEEN.Tween(figure.object.position)
+      .to(
+      {
+        x: 0,
+          y: 0,
+        z: 18,
+      },
+        Math.random() * 1000 + 500,
+      )
+      .easing(TWEEN.Easing.Exponential.InOut)
+      .start();
+  });
+};
+
+window.addEventListener('click', handleFigureClick);
+
+// TODO: добавить кнопочку 'reset' для возвращения сцены к дефолтному состоянию
+
+/** Базовые обработчики событий для поддержки ресайза */
+window.addEventListener(
+  'resize',
+  debounce(() => {
+    // Обновляем размеры
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // Обновляем соотношение сторон камеры
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    // Обновляем renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    renderer.render(scene, camera);
+  }, 200),
+);
+
+window.addEventListener(
+  'dblclick',
+  debounce(() => {
+    if (!document.fullscreenElement) {
+      canvas.requestFullscreen();
+    } else {
+      document.exitFullscreen();
+    }
+  }, 200),
+);
